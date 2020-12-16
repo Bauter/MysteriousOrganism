18dbfbf166574ad10a9ffbe226a0f673cf6c2684
Bauter <bauter09@gmail.com>
2020-12-15 21:41:21 -0600

    completed final function. created function to generate 30 instances of subjects that will survive

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -73,8 +73,10 @@ const pAequorFactory = (num, array) => {
       }
     },
     compareDNA(pAequor) {
+      // log to get a visual of data.
       console.log(`Sample 1: \n ${this.specimenNum} \n ${this.dna}`);
       console.log(`Sample 2: \n ${pAequor.specimenNum} \n ${pAequor.dna}`)
+      // Assign the two arrays to variables, create and empty array for storage, and loop through looking for matching letters at the same index.
       const testSubject1 = this.dna;
       const testSubject2 = pAequor.dna;
       const matchedLetters = []
@@ -84,22 +86,49 @@ const pAequorFactory = (num, array) => {
             matchedLetters.push(testSubject1[i]);
           };
       };
-      console.log(matchedLetters.length)
+      // log to visualize.
+      console.log(`${matchedLetters.length} matching`)
+      // calculate the percent of matching letters, log and return.
       let amountMatching = matchedLetters.length
       let outOf = amountMatching / 15;
       let result = outOf * 100;
       console.log(`${Math.floor(result)}% match between subjects!`);
       return Math.floor(result);
+    },
+    willLikelySurvive() {
+      // create an empty array to store the amount of matching letters.
+      const countMatch = [];
+
+      // assign the dna array to a variable and loop through it looking for 'C' & 'G' strands.
+      const dnaArray = this.dna;
+      for (let i=0; i< dnaArray.length; i++) {
+        if(dnaArray[i] === 'C' || dnaArray[i] === 'G') {
+          countMatch.push(dnaArray[i]);
+        };
+      };
+
+      // calculate result.
+      let amount = countMatch.length;
+      let outOf = amount / 15;
+      let result = outOf * 100;
+
+      // Determine if the result is < or > 60.
+      if(result < 60) {
+        return false;
+      } else {
+        return true;
+      };
     }
   };
 };
 
 // Test Function 1:
 let test1 = pAequorFactory(1, mockUpStrand())
-console.log(`Test 1: \n ${JSON.stringify(test1)}`);
+console.log(`TEST 1: \n ${JSON.stringify(test1)}`);
 
 // Test Function 2:
 let test2 = pAequorFactory(1, mockUpStrand())
+console.log('TEST 2')
 console.log(`Original: \n ${JSON.stringify(test2)}`)
 test2.mutate();
 console.log(`Mutated: \n ${JSON.stringify(test2)}`)
@@ -107,11 +136,35 @@ console.log(`Mutated: \n ${JSON.stringify(test2)}`)
 //Test Function 3:
 let test3 = pAequorFactory(1, mockUpStrand());
 let test4 = pAequorFactory(2, mockUpStrand());
+console.log('TEST 3')
 console.log(JSON.stringify(test3))
 console.log(JSON.stringify(test4))
-test3.compareDNA(test4);
+console.log(test3.compareDNA(test4));
 
+// Test Function 4:
+let test5 = pAequorFactory(1, mockUpStrand());
+console.log('TEST 4')
+console.log(test5.willLikelySurvive());
 
+// Create 30 instances or survivors
+const createSurvivors = () => {
+  // create empty array for storage.
+  const willSurvive = [];
+  // loop until willSurvive holds 30 instances.
+  for(let i = 0; willSurvive.length < 30; i++) {
+    let subject = pAequorFactory(i, mockUpStrand());
+    // Use conditional to determine if they will survive, if so, push to will survive
+    if(subject.willLikelySurvive() == true) {
+      
+      willSurvive.push(subject)
+    };
+
+  };
+  console.log(willSurvive.length)
+  return willSurvive;
+};
+
+console.log(createSurvivors());
 
 
 
